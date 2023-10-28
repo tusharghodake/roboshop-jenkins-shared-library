8993e2cafb0766ed8aa41e3046c9b73be74e8298
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-29 00:39:12 +0530

    code updated

---
+++ vars/ci.groovy
@@ -0,0 +1,37 @@
+
+def call(){
+pipeline{
+
+    agent{
+        label 'workstation'
+    }
+
+    stages{
+
+        stage('compile/build'){
+            steps{
+                echo 'compile/build'
+            }
+        }
+
+        stage('Unit Tests'){
+            steps{
+                echo 'Unit Tests'
+            }
+        }
+
+        stage('Quality Control'){
+            steps{
+                echo 'Quality Control'
+            }
+        }
+
+        stage('Upload Code to centralized place'){
+            steps{
+                echo 'Upload'
+            }
+        }
+
+    }
+}
+}
---
+++ vars/test.groovy
@@ -0,0 +1,29 @@
+@Library('roboshop')
+
+pipeline{
+    agent any
+    stages{
+
+        stage('compile/build'){
+
+            steps{
+
+                script{
+
+                    env.envvar = "environment variable"
+
+                    def id=10
+                    def Name = "Tushar"
+
+                    print "id: ${id}"
+                    print "name: ${Name}"
+                }
+                script{
+
+                    print "name: ${Name}"
+                    print "envvar :${envvar}"
+                }
+            }
+        }
+    }
+}
