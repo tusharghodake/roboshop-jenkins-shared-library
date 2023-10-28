b3a74711fcb903642ef239c3105969ec264beafa
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-29 01:05:14 +0530

    code updated

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,29 +1,29 @@
-@Library('roboshop') _
+def call () {
+    pipeline {
+        agent {
+            label 'workstation'
+        }
+        stages {
 
-pipeline{
-    agent{
-        label 'workstation'
-    }
-    stages{
+            stage('compile/build') {
 
-        stage('compile/build'){
+                steps {
 
-            steps{
+                    script {
 
-                script{
+                        env.envvar = "environment variable"
 
-                    env.envvar = "environment variable"
+                        def id = 10
+                        def Name = "Tushar"
 
-                    def id=10
-                    def Name = "Tushar"
+                        print "id: ${id}"
+                        print "name: ${Name}"
+                    }
+                    script {
 
-                    print "id: ${id}"
-                    print "name: ${Name}"
-                }
-                script{
-
-                    print "name: ${Name}"
-                    print "envvar :${envvar}"
+                        print "name: ${Name}"
+                        print "envvar :${envvar}"
+                    }
                 }
             }
         }
