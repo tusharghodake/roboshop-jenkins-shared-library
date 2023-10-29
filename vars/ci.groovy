4a3ec97e601fa079af0fb4f6865c64825b0c524f
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-29 23:31:53 +0530

    code updated

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -8,7 +8,7 @@ def call() {
             }
             stages {
 
-                stage('compile/build') {
+                stage('Compile/Build') {
                     steps {
                         script {
                             common.compile()
