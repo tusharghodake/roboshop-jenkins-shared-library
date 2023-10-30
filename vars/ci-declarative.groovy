d8459f2d7dc15f5cb0884c9a786bc7ce021f1bc7
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-30 18:35:49 +0530

    code updated

--- vars/ci-declarative.groovy
+++ vars/ci-declarative.groovy
@@ -1,25 +1,53 @@
 def call() {
 
     try {
-        node('workstation2'){
-            stage('Compile/Build'){
-                common.compile()
+        pipeline {
+
+            agent {
+                label 'workstation2'
             }
-            stage('Unit Tests'){
-                common.unittests()
-            }
-            stage('Quality Control'){
-                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]){
-                    sh "sonar-scanner -Dsonar.host.url=http://54.162.159.114:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+            stages {
+
+                stage('Compile/Build') {
+                    steps {
+                        script {
+                            common.compile()
+                        }
+                    }
+                }
+
+                stage('Unit Tests') {
+                    steps {
+                        script {
+                            common.unittests()
+                        }
+                    }
+                }
+
+                stage('Quality Control') {
+                    environment{
+//                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
+                    steps {
+                        script{
+                            SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]){
+                                sh "sonar-scanner -Dsonar.host.url=http://54.162.159.114:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                            }
+                        }
+
+                    }
+                }
+
+                stage('Upload Code to centralized place') {
+                    steps {
+                        echo 'Upload'
+                    }
+                }
+
             }
         }
-            stage('Upload Code to centralized place'){
-                echo 'Upload'
-            }
-            }
-
     } catch (Exception e) {
         common.email("Failed")
     }
--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,53 +1,28 @@
 def call() {
 
     try {
-        pipeline {
-
-            agent {
-                label 'workstation2'
+        node('workstation2'){
+            stage('Compile/Build'){
+                common.compile()
             }
-            stages {
-
-                stage('Compile/Build') {
-                    steps {
-                        script {
-                            common.compile()
-                        }
-                    }
+            stage('Clean'){
+                cleanWs()
+            }
+            stage('Unit Tests'){
+                common.unittests()
+            }
+            stage('Quality Control'){
+                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]){
+                    sh "sonar-scanner -Dsonar.host.url=http://54.162.159.114:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                 }
-
-                stage('Unit Tests') {
-                    steps {
-                        script {
-                            common.unittests()
-                        }
-                    }
-                }
-
-                stage('Quality Control') {
-                    environment{
-//                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                    }
-                    steps {
-                        script{
-                            SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]){
-                                sh "sonar-scanner -Dsonar.host.url=http://54.162.159.114:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
-                            }
-                        }
-
-                    }
-                }
-
-                stage('Upload Code to centralized place') {
-                    steps {
-                        echo 'Upload'
-                    }
-                }
-
+            }
+            stage('Upload Code to centralized place'){
+                echo 'Upload'
             }
         }
+
     } catch (Exception e) {
         common.email("Failed")
     }
