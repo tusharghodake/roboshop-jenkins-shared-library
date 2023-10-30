2f4a0a91318c4246231fc65f4794d8bea4325ab0
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-30 17:43:13 +0530

    code updated

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -30,7 +30,7 @@ def call() {
                         SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                     }
                     steps {
-                        sh 'sonar-scanner -Dsonar.host.url=http://54.162.159.114:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart'
+                        sh "sonar-scanner -Dsonar.host.url=http://54.162.159.114:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
 
                     }
                 }
