8cf1e5cb03ec15d4cd316ca5656b6fb754d77bab
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-11-01 22:37:04 +0530

    code updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -32,7 +32,12 @@ def email(email_note) {
 def artifactPush() {
     sh "echo ${TAG_NAME} > VERSION"
     if (app_lang == "nodejs") {
-    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
-  }
+        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
+    }
     sh 'ls -l'
+    NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+    NEXUS_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
+        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://54.152.27.170:8081/repository/${component}/${component}-${TAG_NAME}.zip"
+    }
 }
